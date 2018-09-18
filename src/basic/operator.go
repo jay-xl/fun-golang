769bf6e957cf4b6f4121df238d780dc513b6eda5
hvkcoder <hvkcoder@163.com>
2018-09-18 15:54:58 +0800

    Update Introduction to golang

--- src/basic/operator.go
+++ src/basic/operator.go
@@ -1,1 +1,23 @@
 package basic
+
+import "fmt"
+
+// 算数运算符
+func ArithmeticOperator() {
+	count := 0
+	fmt.Printf("%d + 1 = %d \n", count, count+1)
+	fmt.Printf("%d - 1 = %d \n", count, count-1)
+	fmt.Printf("%d * 1 = %d \n", count, count*1)
+	fmt.Printf("%d / 1 = %d \n", count, count/1)
+	fmt.Println(count, "% 1 = ", count%1)
+	count++
+	fmt.Println("count 自增值 = ", count)
+	count--
+	fmt.Println("count 自减值 = ", count)
+}
+
+// 逻辑运算符
+func RelationalOperator(){
+	fmt.Println("1 == 1", 1 == 1, "1 == 2", 1 == 2)
+	fmt.Println("1 != 1", 1 != 1, "1 != 2", 1 != 2)
+}
--- src/main.go
+++ src/main.go
@@ -3,8 +3,10 @@ package main
 import "./basic"
 
 func main(){
-	basic.JumpValueByIota()
-	basic.InsertValueByIota()
-	basic.ExpressionByIota()
-	basic.SingleLineByIota()
+	//basic.JumpValueByIota()
+	//basic.InsertValueByIota()
+	//basic.ExpressionByIota()
+	//basic.SingleLineByIota()
+	basic.ArithmeticOperator()
+	basic.RelationalOperator()
 }
